4bb287c1ffb9acdb3cf6242f90c9a0d05b8fee2a
Ognyanka Boneva <ogi.boneva@gmail.com>
2021-06-20 22:21:14 +0300

    Add command line args parsing

---
+++ commandline.py
@@ -0,0 +1,25 @@
+
+import sys
+import getopt
+from configs import data_config, train_config
+
+
+def parse_args(argv):
+    try:
+        opts, args = getopt.getopt(
+            argv, "t:e:d:", ["trainbs=", "epochs=", "datadir="])
+    except getopt.GetoptError:
+        print(
+            'main.py -t <train_batch_size> -e <epochs> -d <datadir>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print(
+                'main.py -t <train_batch_size> -e <epochs> -d <datadir>')
+            sys.exit()
+        elif opt in ("-t", "--trainbs"):
+            data_config.train_batch_size = int(arg)
+        elif opt in ("-e", "--epochs"):
+            train_config.epochs = int(arg)
+        elif opt in ("-d", "--datadir"):
+            data_config.data_dir = arg
